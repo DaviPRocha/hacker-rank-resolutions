0d0d9df22382ac2b3448c2981b75b9fc87980548
DaviPRocha <73966030+DaviPRocha@users.noreply.github.com>
2023-05-18 15:07:44 -0300

    Create Insertion Sort - Part 2.js

---
+++ Insertion Sort - Part 2.js
@@ -0,0 +1,18 @@
+function insertionSort2(n, arr) {
+    // Write your code here
+    for(let i=1; i<n; i++) {
+        let j=i-1
+        let x
+        while(j>=0) {
+            if((!x || arr[x] > arr[j]) && arr[j] > arr[i]) {
+                x = j
+            }
+            j--
+        }
+        if(x !== undefined) {
+            let z = arr.splice(i, 1)
+            arr.splice(x, 0, z[0])
+        }
+        console.log(arr.join(' '))
+    }
+}
